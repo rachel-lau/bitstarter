f48ad36e1f6ffb8a8122b6bfffb76ade63d224b5
Rachel Lau <rachel.yw.lau@gmail.com>
2013-07-05 13:42:14 -0700

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -4,8 +4,10 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
+var buf = new Buffer(256);
+
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html'));
+    response.send(buf.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
